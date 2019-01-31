162c99761a11bb0b703105459e506b7db6038747
Chris <chrsschb@yahoo.com>
2019-01-30 22:55:56 -0500

    Update MainForm.cs

--- EQEmu Patcher/EQEmu Patcher/MainForm.cs
+++ EQEmu Patcher/EQEmu Patcher/MainForm.cs
@@ -22,8 +22,8 @@ namespace EQEmu_Patcher
          *  EDIT THESE VARIABLES FOR EACH SERVER
          * 
          ****/
-        public static string serverName = "Rebuild EQ";
-        public static string filelistUrl = "http://rebuildeq.com/patch/";
+        public static string serverName = "Clumsy's World: Resurgence";
+        public static string filelistUrl = "https://patch.clumsysworld.com";
         public static bool defaultAutoPlay = false; //When a user runs this first time, what should Autoplay be set to?
         public static bool defaultAutoPatch = false; //When a user runs this first time, what should Autopatch be set to?
 
